eedd75262ba033b2ade930273eba79f93c9ae632
Manal Bouriba <m.bouriba@esi-sba.dz>
2023-04-06 14:29:17 +0100

    transferred to local font serving

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import './fonts/Inter.ttf';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
