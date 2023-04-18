a0922877bf39cf90fe54fb7e18cdf243ddb9b3b4
Manal Bouriba <m.bouriba@esi-sba.dz>
2023-04-18 18:12:45 +0100

    fixed font

--- pages/_app.js
+++ pages/_app.js
@@ -12,16 +12,24 @@ import '../src/navBar/navBar.css'
 import '../src/results/results.css'
 import '../src/sponsoredCompany/sponsoredCompany.css'
 import '../src/app.css'
+import { Inter } from 'next/font/google'
 import { useEffect } from "react";
 
-
+const inter = Inter({ subsets: ['latin'] , weight: ['400', '700'],})
 
 
 // This default export is required in a new `pages/_app.js` file.
 export default function App({ Component, pageProps }) {
+  
   useEffect(()=>{
     require("bootstrap/dist/css/bootstrap.min.css");
     require("bootstrap/dist/js/bootstrap.bundle.min");
  },[])
-  return <Component {...pageProps} />
+  return(    <>
+    <style jsx global>{`
+      .inter {
+        font-family: ${inter.style.fontFamily};
+      }
+    `}</style><Component {...pageProps} /></>)
+
 }
